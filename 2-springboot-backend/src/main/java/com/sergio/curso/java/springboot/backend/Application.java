5007828940b787587ae4b8e6313d9929af9590c6
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-20 12:01:10 -0600

    Creando proyecto y logica de negocio

---
+++ 2-springboot-backend/src/main/java/com/sergio/curso/java/springboot/backend/Application.java
@@ -0,0 +1,13 @@
+package com.sergio.curso.java.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
