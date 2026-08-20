6eac21a4c48039e06edad62849e8c3ac2451699f
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-20 23:15:19 +0000

    Componente Table Creado

--- 3-react-product/src/ProductsApp.jsx
+++ 3-react-product/src/ProductsApp.jsx
@@ -1,12 +1,34 @@
 import PropTypes from "prop-types"
 import { ProductTable } from "./componentes/ProductTable"
+import { useEffect, useState } from "react"
+
+const initProducts = [{
+  id: 1,
+  name: 'Monitor Asus 37 pulgadas',
+  description: "El monitor es perfecto para juegos",
+  price: 1000
+},{
+  id: 2,
+  name: 'Iphone 16 pro',
+  description: "El telefono es excelente e incluye Apple Intelligence!",
+  price: 45000
+},
+]
 
 export const ProductsApp = ({title}) => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    setProducts(initProducts);
+    console.log('cargando la página ...')
+  }, [])
+
   return <div className='container my-4' >
     <h2>{title}</h2>
     <div className="row">
       <div className="col">
-          <ProductTable />
+          <ProductTable products={products} />
       </div>
     </div>
   </div>
--- 3-react-product/src/componentes/ProductTable.jsx
+++ 3-react-product/src/componentes/ProductTable.jsx
@@ -1,4 +1,6 @@
-export const ProductTable = () => {
+import PropTypes from "prop-types"
+
+export const ProductTable = ({products}) => {
     return <table  className="table table-hover table-striped">
         <thead>
             <tr>
@@ -9,13 +11,24 @@ export const ProductTable = () => {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            {
+                products.map(product => {
+                    return (
+                        <tr key={product.id}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>{product.description}</td>
+                            <td>{product.price}</td>
+                        </tr>
+                    )
+                })
+            }
+            
         </tbody>
 
     </table>
+}
+
+ProductTable.propTypes = {
+    products: PropTypes.array.isRequired
 }
